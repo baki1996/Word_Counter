91099373a891b06d39580aafc38d060340fd99d1
Bakhtawar khan <160532125+baki1996@users.noreply.github.com>
2024-04-17 22:41:37 +0500

    push my project

---
+++ index.js
@@ -0,0 +1,15 @@
+//import the "inquirer" module,which is command line interface for Node js,
+import inquirer from "inquirer";
+//Declare a constant 'answers' and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "Input",
+        message: "Enter your sentence to count the word"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+//print the array of words to the console
+console.log(words);
+//print the word count of the sentence to the console
+console.log(`your Sentence word count is ${words.length}`);
---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+//this line is called shabang,it tells the OS for the node js
+
+//import the "inquirer" module,which is command line interface for Node js,
+import inquirer from "inquirer"
+
+//Declare a constant 'answers' and assign it to the result of inquirer.prompt function
+
+const answers: {
+    Sentence:string
+} =await inquirer.prompt(
+    [
+        {
+            name: "Sentence",
+            type: "Input",
+            message: "Enter your sentence to count the word"
+
+        }
+    ]
+);
+const words = answers.Sentence.trim().split(" ")
+//print the array of words to the console
+console.log(words);
+
+//print the word count of the sentence to the console
+console.log(`your Sentence word count is ${words.length}`);
